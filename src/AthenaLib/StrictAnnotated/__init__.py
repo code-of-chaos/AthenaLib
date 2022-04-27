4b3ebe8453f6d2247cd9e5ed71da38be96473d1a
DirectiveAthena <directive.athena@gmail.com>
2022-04-27 17:58:11 +0200

    Half Broken StrictAnnotated_Full

--- src/AthenaLib/StrictAnnotated/__init__.py
+++ src/AthenaLib/StrictAnnotated/__init__.py
@@ -7,10 +7,12 @@
 
 # Custom Packages
 from .StrictAnnotated import StrictAnnotated, StrictError, StrictAnnotatedMethod
+from .StrictAnnotated_Full import StrictAnnotated_Full, StrictAnnotatedMethod_Full
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - All -
 # ----------------------------------------------------------------------------------------------------------------------
 __all__=[
-    "StrictAnnotated", "StrictError", "StrictAnnotatedMethod"
+    "StrictAnnotated", "StrictError", "StrictAnnotatedMethod",
+    "StrictAnnotated_Full","StrictAnnotatedMethod_Full"
 ]
