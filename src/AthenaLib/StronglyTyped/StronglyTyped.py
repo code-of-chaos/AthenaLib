39f9c111c5ea809c3e0189d3ab8f760d7b9dad5f
DirectiveAthena <directive.athena@gmail.com>
2022-04-27 08:52:15 +0200

    Comments

--- src/AthenaLib/StronglyTyped/StronglyTyped.py
+++ src/AthenaLib/StronglyTyped/StronglyTyped.py
@@ -19,12 +19,18 @@ def StronglyTyped(fnc):
     #   as now it is only done once, instead of every function
     fncspec = inspect.getfullargspec(fnc)
     def wrapper(*args, **kwargs):
-        for k, v in zip(
-            (*fncspec.args, *kwargs),
-            (*args,*kwargs.values())
-        ):
-            if not isinstance(v, fncspec.annotations[k]):
-                raise StrongError(f"{v} was not the same type as the Strongly typed: {fncspec.annotations[k]}")
+        # Only run the check if there are actually are any annotations
+        #   Else you just loose time
+        if fncspec.annotations:
+            for k, v in zip((*fncspec.args, *kwargs),(*args,*kwargs.values())):
+                # If no annotations are placed on the function,
+                #   then the given variable will not be strongly typed
+                if not isinstance(v, fncspec.annotations[k]):
+                    raise StrongError(
+                        f"'{v}' was not the same type as the expected Strongly typed: '{fncspec.annotations[k]}'"
+                    )
+        # Run the actual function
+        #   Won't reach this point if the 'StrongError' was raised
         return fnc(*args,**kwargs)
     return wrapper
 
@@ -32,12 +38,20 @@ def StronglyTypedMethod(fnc):
     # do the inspection before a function is esxecuted
     #   as now it is only done once, instead of every function
     fncspec = inspect.getfullargspec(fnc)
+    fncspec_args = [a for a in fncspec.args if a != 'self']
     def wrapper(self, *args, **kwargs):
-        for k, v in zip(
-            (*(a for a in fncspec.args if a != 'self'), *kwargs),
-            (*args,*kwargs.values())
-        ):
-            if not isinstance(v, fncspec.annotations[k]):
-                raise StrongError(f"{v} was not the same type as the Strongly typed: {fncspec.annotations[k]}")
+        # Only run the check if there are actually are any annotations
+        #   Else you just loose time
+        if fncspec.annotations:
+            for k, v in zip((*fncspec_args, *kwargs),(*args,*kwargs.values())
+            ):
+                # If no annotations are placed on the function,
+                #   then the given variable will not be strongly typed
+                if not isinstance(v, fncspec.annotations[k]):
+                    raise StrongError(
+                        f"'{v}' was not the same type as the expected Strongly typed: '{fncspec.annotations[k]}'"
+                    )
+        # Run the actual function
+        #   Won't reach this point if the 'StrongError' was raised
         return fnc(self, *args,**kwargs)
     return wrapper
