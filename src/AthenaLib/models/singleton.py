4561f88b29bf5be27f06abffe63d268fddbd2613
Andreas Sas <directive.athena@gmail.com>
2022-06-11 11:22:35 +0100

    Fix; Singleton works as expected

--- src/AthenaLib/models/singleton.py
+++ src/AthenaLib/models/singleton.py
@@ -16,7 +16,9 @@ class Singleton:
     A class which inherits from this class, will only be able to be created once in the run.
     This means, if a user tries to call a new instance of the class, it will simply return the first created class.
     """
+
     def __new__(cls, *args,**kwargs):
         if not hasattr(cls, '_instance'):
-            cls._instance = super(Singleton, cls).__new__(cls).__init__(*args,**kwargs)
+            cls._instance = super(Singleton, cls).__new__(cls)
+            cls._instance.__init__(*args,**kwargs)
         return cls._instance
