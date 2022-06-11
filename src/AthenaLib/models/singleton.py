6405e98c28f880798c9868a2a0bc28f8cc8a1b79
Andreas Sas <directive.athena@gmail.com>
2022-06-11 10:35:22 +0100

    Docu

--- src/AthenaLib/models/singleton.py
+++ src/AthenaLib/models/singleton.py
@@ -12,6 +12,10 @@ from __future__ import annotations
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 class Singleton:
+    """
+    A class which inherits from this class, will only be able to be created once in the run.
+    This means, if a user tries to call a new instance of the class, it will simply return the first created class.
+    """
     def __new__(cls):
         if not hasattr(cls, '_instance'):
             cls._instance = super(Singleton, cls).__new__(cls)
