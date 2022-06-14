7ad54dd8d2b92c4854fbb72552e6a392eca5edc0
Andreas Sas <directive.athena@gmail.com>
2022-06-14 11:06:53 +0100

    Update test_vectors.py

--- Tests/models/test_vectors.py
+++ Tests/models/test_vectors.py
@@ -18,10 +18,12 @@ class TestVectors(TestStructure):
         self.subtest_multiple_cases(
             ((1,),lambda : Vector1D(1).export()),
         )
+
     def test_Vector2D(self):
         self.subtest_multiple_cases(
             ((1,2),lambda : Vector2D(1,2).export()),
         )
+
     def test_Vector3D(self):
         self.subtest_multiple_cases(
             ((1,2,3),lambda : Vector3D(1,2,3).export()),
