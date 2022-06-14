775c1dfbb7af70b85629a4d67b47899a7db85ac9
DirectiveAthena <directive.athena@gmail.com>
2022-06-14 15:14:07 +0200

    Feat: Package import detection

--- src/AthenaLib/fixes/asyncio.py
+++ src/AthenaLib/fixes/asyncio.py
@@ -12,5 +12,8 @@
 # ----------------------------------------------------------------------------------------------------------------------
 def fix_nested_asyncio():
     """Simple function to create nested asyncio loops"""
-    import nest_asyncio
-    nest_asyncio.apply()
+    try:
+        import nest_asyncio
+        nest_asyncio.apply()
+    except ImportError:
+        raise ImportError("nest-asyncio package has to be installed for this to work")
