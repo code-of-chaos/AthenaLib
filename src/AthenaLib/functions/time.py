c7349b6672eeb9b5cb761d831af4aacf1fdaab14
DirectiveAthena <directive.athena@gmail.com>
2022-06-15 21:50:04 +0200

    Feat: convert_time_to_seconds

---
+++ src/AthenaLib/functions/time.py
@@ -0,0 +1,30 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+
+# Custom Library
+
+# Custom Packages
+from AthenaLib.models.time import Second, Minute, Hour
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+def convert_time_to_seconds(time:Second|Minute|Hour, to_int:bool=False) -> Second|int:
+    if isinstance(time, Second):
+        if to_int:
+            return int(time)
+        return time
+    elif isinstance(time, Minute):
+        if to_int:
+            return int(time)*60
+        return Second(int(time)*60)
+    elif isinstance(time, Hour):
+        if to_int:
+            return int(time)*3600
+        return Second(int(time)*3600)
+    else:
+        return NotImplemented(time)
+
