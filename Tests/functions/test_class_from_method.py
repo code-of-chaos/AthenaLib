307317dcdae0da65bb9e67eb826696e57ba260fa
DirectiveAthena <directive.athena@gmail.com>
2022-06-17 15:09:53 +0200

    class_from_method
    Brought to you by TotalEcl1ps3

--- Tests/functions/test_class_from_method.py
+++ Tests/functions/test_class_from_method.py
@@ -14,24 +14,49 @@ from Tests.test_structure import TestStructure
 # - Support Code -
 # ----------------------------------------------------------------------------------------------------------------------
 class A:
+    b_str:str
+    def __init__(self):
+        self.b_str = "b has been called"
     def a(self):
         pass
+
+    @class_from_method.class_from_method_decorator
+    def b(self):
+        return self.b_str
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 class TestClassFromMethod(TestStructure):
     def test_get_class_from_bound_method(self):
-        print(
-            class_from_method.get_class_from_bound_method(A().a)
+        self.assertIs(
+            A, class_from_method.get_class_from_bound_method(A().a)
         )
     def test_get_class_from_unbound_method(self):
-        print(
-            class_from_method.get_class_from_unbound_method(A.a)
+        self.assertIs(
+            A, class_from_method.get_class_from_unbound_method(A.a)
         )
-    def test_test_get_class_from_method(self):
-        print(
-            class_from_method.get_class_from_method(A().a)
+
+    def test_get_class_from_method(self):
+        self.assertIs(
+            A, class_from_method.get_class_from_bound_method(A().a)
         )
-        print(
-            class_from_method.get_class_from_method(A.a)
+        self.assertIs(
+            A, class_from_method.get_class_from_unbound_method(A.a)
+        )
+        self.assertIs(
+            A, class_from_method.get_class_from_method(A().a)
+        )
+        self.assertIs(
+            A, class_from_method.get_class_from_method(A.a)
+        )
+
+    def test_get_class_from_method_decorator(self):
+        self.assertIs(
+            A, A.b.owner
+        )
+        self.assertIs(
+            A, A().b.owner
+        )
+        self.assertEqual(
+            "b has been called", A().b()
         )
