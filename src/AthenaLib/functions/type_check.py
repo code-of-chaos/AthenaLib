96a667b5702c9490554790b8b9a2bfed3a955f51
DirectiveAthena <directive.athena@gmail.com>
2022-07-04 13:32:50 +0200

    Feat: reworked HTML elements

--- src/AthenaLib/functions/type_check.py
+++ src/AthenaLib/functions/type_check.py
@@ -3,6 +3,7 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # General Packages
 from __future__ import annotations
+from typing import Union, Any
 
 # Custom Library
 
@@ -11,3 +12,8 @@ from __future__ import annotations
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
+def type_check_error(obj:object, types:Union[type,...]|type) -> Any:
+    if not isinstance(obj, types):
+        raise TypeError(f"{obj} was not of the allowed types of: {types}")
+    return obj
+
