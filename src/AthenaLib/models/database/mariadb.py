4d9959cac1cfe11f30148eeb27fffb6035f61f0c
DirectiveAthena <directive.athena@gmail.com>
2022-08-09 23:50:27 +0200

    feat: Crude Database Mariadb

---
+++ src/AthenaLib/models/database/mariadb.py
@@ -0,0 +1,39 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+import aiomysql
+import asyncio
+from dataclasses import dataclass, field
+
+# Custom Library
+
+# Custom Packages
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+@dataclass(slots=True)
+class DatabaseMariaDB:
+    host:str
+    port:int
+    pool:aiomysql.Pool = field(init=False)
+
+    async def connect(self,user:str,password:str, loop=asyncio.AbstractEventLoop):
+        self.pool = await aiomysql.create_pool(
+            host=self.host,
+            port=self.port,
+            user=user,
+            password=password,
+            loop=loop
+        )
+
+    async def acquire_cursor(self) -> aiomysql.Cursor:
+        async with self.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                yield cursor
+
+    async def close(self):
+        self.pool.close()
+        await self.pool.wait_closed()
