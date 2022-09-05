64aa31dd5494f401a39d8e763600b82779423bab
DirectiveAthena <directive.athena@gmail.com>
2022-09-05 16:06:11 +0200

    Update arrays.py

--- src/AthenaLib/math/arrays.py
+++ src/AthenaLib/math/arrays.py
@@ -15,6 +15,10 @@ import math
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 def calculate_largest_radius(center_point:ArrayLike, outside_points:ArrayLike) -> float:
+    """
+    Determines the largest radius of all points compared to a center point.
+    Both the center_point and the outside_points should be Numpy Arrays
+    """
     return np.amax(
         np.array([
             calculate_distance_between_two_coords(center_point, coord)
@@ -24,12 +28,19 @@ def calculate_largest_radius(center_point:ArrayLike, outside_points:ArrayLike) -
     )
 
 def calculate_distance_between_two_coords(point_1:ArrayLike, point_2:ArrayLike) -> float:
+    """
+    Determines the distance between two points which are stored as Numpy arrays.
+    """
     return math.sqrt(
         math.pow(point_1[0] - point_2[0] , 2)
         + math.pow(point_1[1] - point_2[1], 2)
     )
 
 def calculate_center(points:ArrayLike) -> ArrayLike:
+    """
+    Returns a Numpy Array which is equal distance away from the Array of points.
+    Points is a Numpy Array of Numpy Arrays which describe the 2d points
+    """
     return np.array([
         points[:, 0].sum() / (length := len(points)) ,
         points[:, 1].sum() / length
