e7accbf07769ed9821159ff61fc6367ea9762e01
DirectiveAthena <directive.athena@gmail.com>
2022-09-29 14:35:27 +0200

    Feat: Simple Json functions

---
+++ src/AthenaLib/general/functions/json.py
@@ -0,0 +1,27 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+import json
+import dataclasses
+
+# Custom Library
+from AthenaLib.constants.types import PATHLIKE
+
+# Custom Packages
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+def load_jsonfile(filepath:PATHLIKE) -> dict:
+    with open(filepath, "r") as file:
+        return json.load(file)
+
+def dump_dataclass_to_jsonfile(obj:dataclasses.dataclass, filepath:PATHLIKE):
+    with open(filepath, "w+") as file:
+        json.dump(
+            obj=dataclasses.asdict(obj),
+            fp=file,
+
+        )
