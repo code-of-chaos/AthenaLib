bc010ea630eb5264b856ecd6573cd3f525b60901
DirectiveAthena <directive.athena@gmail.com>
2022-11-24 21:18:48 +0100

    Feat: log_track and docs

--- src/AthenaLib/logging/_logger.py
+++ src/AthenaLib/logging/_logger.py
@@ -18,6 +18,7 @@ error_type = lambda obj: f"the following did not inherit from expected AthenaLog
 error_logger = lambda obj: f"the following did not inherit from expected AthenaLogger: {obj}"
 
 class LoggerLevels(enum.StrEnum):
+    TRACK = enum.auto()
     DEBUG = enum.auto()
     WARN = enum.auto()
     ERROR = enum.auto()
@@ -35,13 +36,29 @@ class AthenaLogger(ABC):
     def log(self, level:LoggerLevels, section:str|enum.StrEnum, text:str|None):
         pass
 
+    def log_track(self, section:str|enum.StrEnum, text:str|None=None):
+        """
+        Simple log command that is meant for data that needs to be tracked while in development mode.
+        Should be disabled once sufficient data has been gathered throughout development
+        """
+        self.log(level=LoggerLevels.TRACK, section=section, text=text)
+
     def log_debug(self, section:str|enum.StrEnum, text:str|None=None):
+        """
+        Simple log command for debug data
+        """
         self.log(level=LoggerLevels.DEBUG, section=section, text=text)
 
     def log_warning(self, section:str|enum.StrEnum, text:str|None=None):
+        """
+        Simple log command for Warnings
+        """
         self.log(level=LoggerLevels.WARN, section=section, text=text)
 
     def log_error(self, section:str|enum.StrEnum, text:str|None=None):
+        """
+        Simple log command for Errors
+        """
         self.log(level=LoggerLevels.ERROR, section=section, text=text)
 
 
