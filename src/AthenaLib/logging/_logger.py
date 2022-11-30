635fe0bf3ec2d3bc33da2e07885a97d9e456fa93
DirectiveAthena <directive.athena@gmail.com>
2022-11-30 21:32:10 +0100

    Refactor: SqliteLogger simplified to one file

--- src/AthenaLib/logging/_logger.py
+++ src/AthenaLib/logging/_logger.py
@@ -7,7 +7,7 @@ from abc import ABC, abstractmethod
 import enum
 import concurrent.futures
 from dataclasses import dataclass, field
-from typing import Callable, Any
+from typing import Any, Self
 
 # Athena Packages
 
@@ -19,7 +19,7 @@ from typing import Callable, Any
 error_type = lambda obj: f"the following did not inherit from expected AthenaLoggerType: {obj}"
 error_logger = lambda obj: f"the following did not inherit from expected AthenaLogger: {obj}"
 
-class LoggerLevels(enum.StrEnum):
+class LoggerLevel(enum.StrEnum):
     TRACK = enum.auto()
     DEBUG = enum.auto()
     WARN = enum.auto()
@@ -37,14 +37,16 @@ class AthenaLogger(ABC):
 
     _pool_executor: concurrent.futures.ProcessPoolExecutor = field(init=False, default_factory=lambda:concurrent.futures.ProcessPoolExecutor(max_workers=1))
     _logger_entered:bool = field(init=False, default=False)
-    _buffer:list[tuple[LoggerLevels, str|enum.StrEnum, str|None]] = field(init=False, default_factory=list)
+    _buffer:list[tuple[LoggerLevel, str | enum.StrEnum, str | None]] = field(init=False, default_factory=list)
 
-    def __enter__(self):
+    def __enter__(self) -> Self:
         self._logger_entered = True
         for level, section, text in self._buffer:
             self.log(level, section, text)
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         # Makes sure the pool executor is shot down,
         #   else the process will keep to exist and errors might occur
         self._pool_executor.shutdown()
@@ -53,37 +55,37 @@ class AthenaLogger(ABC):
     # - Abstract Features that need to be implemented -
     # ------------------------------------------------------------------------------------------------------------------
     @abstractmethod
-    def log(self, level:LoggerLevels, section:str|enum.StrEnum, data:Any|None):
+    def log(self, level:LoggerLevel, section: str | enum.StrEnum, data:Any):
         pass
 
-    def log_track(self, section:str|enum.StrEnum, data:Any|None=None):
+    def log_track(self, section:str|enum.StrEnum, data:Any=None):
         """
         Simple log command that is meant for data that needs to be tracked while in development mode.
         Should be disabled once sufficient data has been gathered throughout development
         """
         if self.enable_track:
-            self.log(level=LoggerLevels.TRACK, section=section, data=data)
+            self.log(level=LoggerLevel.TRACK, section=section, data=data)
 
-    def log_debug(self, section:str|enum.StrEnum, data:Any|None=None):
+    def log_debug(self, section:str|enum.StrEnum, data:Any=None):
         """
         Simple log command for debug data
         """
         if self.enable_debug:
-            self.log(level=LoggerLevels.DEBUG, section=section, data=data)
+            self.log(level=LoggerLevel.DEBUG, section=section, data=data)
 
-    def log_warning(self, section:str|enum.StrEnum, data:Any|None=None):
+    def log_warning(self, section:str|enum.StrEnum, data:Any=None):
         """
         Simple log command for Warnings
         """
         if self.enable_warning:
-            self.log(level=LoggerLevels.WARN, section=section, data=data)
+            self.log(level=LoggerLevel.WARN, section=section, data=data)
 
-    def log_error(self, section:str|enum.StrEnum, data:Any|None=None):
+    def log_error(self, section:str|enum.StrEnum, data:Any=None):
         """
         Simple log command for Errors
         """
         if self.enable_error:
-            self.log(level=LoggerLevels.ERROR, section=section, data=data)
+            self.log(level=LoggerLevel.ERROR, section=section, data=data)
 
 
 
--- src/AthenaLib/logging/logger_sqlite.py
+++ src/AthenaLib/logging/logger_sqlite.py
@@ -3,16 +3,63 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # General Packages
 from __future__ import annotations
+from dataclasses import dataclass
+from typing import Callable, Self,Any
+import contextlib
 import pathlib
 import enum
-from dataclasses import dataclass
-from typing import Callable
+import sqlite3
 
 # Athena Packages
 
 # Local Imports
-from AthenaLib.logging._logger import AthenaLogger, LoggerLevels
-import AthenaLib.logging.logger_sqlite_functions as LSF
+from AthenaLib.logging._logger import AthenaLogger, LoggerLevel
+from AthenaLib.general.sql import sanitize_sql
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Support Code -
+# ----------------------------------------------------------------------------------------------------------------------
+@contextlib.contextmanager
+def _connect(path:pathlib.Path, *, commit: bool = True, **kwargs) -> None:
+    """
+    Async Context manager to easily connect to the database
+    Commits all changes to the db, before closing the connection
+    """
+    with sqlite3.connect(path,isolation_level=None if commit else "DEFERRED",**kwargs) as db:
+        db.row_factory = sqlite3.Row
+
+        try:
+            yield db
+
+        except sqlite3.Error:
+            db.rollback()
+            raise
+
+        else:
+            # only commit if there were no errors,
+            #   Otherwise always rollback to previous state
+            db.commit() if commit else db.rollback()
+
+def _db_create(path:pathlib.Path, queries:str) -> None:
+    """
+    Method is run by the `bot_constructor` on startup, as it creates the tables the logger needs,
+    but only if they don't exist already
+    """
+    with _connect(path, commit=True) as db:
+        for sql in queries:
+            db.executescript(sql)
+
+def _execute_log(path:pathlib.Path, level:LoggerLevel, section: str | enum.StrEnum, data:Any, table_to_use:str, commit:bool=True) -> None:
+    with _connect(path, commit=commit) as db:
+        # noinspection SqlNoDataSourceInspection,SqlResolve
+        db.execute(f"""
+            INSERT INTO {table_to_use} (lvl, section, txt)
+            VALUES (
+                '{sanitize_sql(level)}', 
+                '{sanitize_sql(section)}', 
+                {"Null" if data is None else f"'{sanitize_sql(data)}'"}
+            );"""
+       )
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
@@ -26,14 +73,14 @@ class AthenaSqliteLogger(AthenaLogger):
     # ------------------------------------------------------------------------------------------------------------------
     # - Context manager for ease of use -
     # ------------------------------------------------------------------------------------------------------------------
-    def __enter__(self):
-        # Check if the file exsists
+    def __enter__(self) -> Self:
+        # Check if the file exists
         if not self.sqlite_path.exists():
             raise FileNotFoundError(self.sqlite_path)
 
         # Makes sure the table will exist
         self._pool_executor.submit(
-            LSF.db_create,
+            _db_create,
             path=self.sqlite_path,
             queries=[
                 f"""# noinspection SqlNoDataSourceInspectionForFile
@@ -50,12 +97,12 @@ class AthenaSqliteLogger(AthenaLogger):
 
         # After the table has been created
         #   Go through the buffer
-        super(AthenaSqliteLogger, self).__enter__()
+        return super(AthenaSqliteLogger, self).__enter__()
 
     # ------------------------------------------------------------------------------------------------------------------
     # - Logger functions that write to the database -
     # ------------------------------------------------------------------------------------------------------------------
-    def log(self, level: LoggerLevels, section: str | enum.StrEnum, data: str | None):
+    def log(self, level: LoggerLevel, section: str | enum.StrEnum, data: str | None) -> None:
         # If the logger itself hasn't been entered yet,
         #   Store the log to the buffer, and execute at a later date
         if not self._logger_entered:
@@ -64,7 +111,7 @@ class AthenaSqliteLogger(AthenaLogger):
 
         # Let the pool execute the call to the database
         self._pool_executor.submit(
-            LSF.execute_log,
+            _execute_log,
             path=self.sqlite_path,
             level=level,
             section=section,
